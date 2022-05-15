6ec0d6813c4df697f0387b48fb96b790b48fcc56
ask <asklko2004@gmail.com>
2022-05-16 01:31:57 +0530

    firt

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,21 @@ Route::middleware('auth')->group(function()
 
     Route::view('/home', 'home')->name('home');
 
-    Route::post('/add-project', [\App\Http\Controllers\ProjectController::class, 'add']);
-    Route::get('/get-projects', [\App\Http\Controllers\ProjectController::class, 'get']);
-    Route::get('/get-active-projects', [\App\Http\Controllers\ProjectController::class, 'getActive']);
+    Route::post('add-project', [\App\Http\Controllers\ProjectController::class, 'add']);
+    Route::get('get-projects', [\App\Http\Controllers\ProjectController::class, 'get']);
+    Route::get('get-active-projects', [\App\Http\Controllers\ProjectController::class, 'getActive']);
 
-    Route::post('/add-task', [\App\Http\Controllers\TaskController::class, 'add']);
-    Route::get('/get-tasks', [\App\Http\Controllers\TaskController::class, 'get']);
-    Route::get('/getTask/{task}', [\App\Http\Controllers\TaskController::class, 'getTask']);
+    Route::post('add-task', [\App\Http\Controllers\TaskController::class, 'add']);
+    Route::get('get-tasks', [\App\Http\Controllers\TaskController::class, 'get']);
+    Route::get('getTask/{task}', [\App\Http\Controllers\TaskController::class, 'getTask']);
 
-    Route::post('/add_task_status', [\App\Http\Controllers\TaskStatusController::class, 'add']);
-    Route::get('/get_task_statuses', [\App\Http\Controllers\TaskStatusController::class, 'get']);
-    Route::get('/get_active_task_statuses', [\App\Http\Controllers\TaskStatusController::class, 'getActive']);
+    Route::post('add_task_status', [\App\Http\Controllers\TaskStatusController::class, 'add']);
+    Route::get('get_task_statuses', [\App\Http\Controllers\TaskStatusController::class, 'get']);
+    Route::get('get_active_task_statuses', [\App\Http\Controllers\TaskStatusController::class, 'getActive']);
 
-    Route::post('/uploadAttachment', [\App\Http\Controllers\ImageController::class, 'uploadAttachment']);
+    Route::post('uploadAttachment', [\App\Http\Controllers\ImageController::class, 'uploadAttachment']);
 
-    Route::post('/addComment', [\App\Http\Controllers\CommentController::class, 'add']);
+    Route::post('addComment', [\App\Http\Controllers\CommentController::class, 'add']);
 });
 
 Auth::routes();
