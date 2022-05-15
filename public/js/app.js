f6130615f5665f6e1115d589d6fe29bf86c76c89
ask <asklko2004@gmail.com>
2022-05-16 01:28:45 +0530

    firt

--- public/js/app.js
+++ public/js/app.js
@@ -19571,7 +19571,7 @@ __webpack_require__.r(__webpack_exports__);
       var _this = this;
 
       _this.loader = true;
-      axios.post('/apis/uploadAttachment/', {
+      axios.post('/uploadAttachment/', {
         'file': _this.file,
         'model': 'Task',
         'id': _this.id
